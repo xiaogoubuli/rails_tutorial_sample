886d261dd87c3324e225387f944a0bf8ce991357
sunfx <sunfx@sun.cn>
2013-09-14 12:34:03 +0800

    full title

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,15 +1,19 @@
 require 'spec_helper'
 
 describe "Static pages" do
-  let(:title_prefix) {"Ruby on Rails Tutorial Sample App | "}
+  let(:base_title) {"Ruby on Rails Tutorial Sample App"}
   describe "Home page" do
     it "should have the content 'Sample App'" do
       visit '/static_pages/home'
       expect(page).to have_content('Sample App')
     end
-    it "should have the title 'Home'" do
+    it "should have the base title" do
       visit '/static_pages/home'
-      expect(page).to have_title("#{title_prefix}Home")
+      expect(page).to have_title("#{base_title}")
+    end
+    it "should not have a custom page title" do
+      visit '/static_pages/home'
+      expect(page).not_to have_title(' | Home')
     end
   end
   describe "Help page" do
@@ -17,9 +21,9 @@ describe "Static pages" do
       visit '/static_pages/help'
       expect(page).to have_content('Help')
     end
-    it "should have the title 'Help'" do
+    it "should have the base title" do
       visit '/static_pages/help'
-      expect(page).to have_title("#{title_prefix}Help")
+      expect(page).to have_title("#{base_title}")
     end
   end
   describe "About page" do
@@ -27,9 +31,9 @@ describe "Static pages" do
       visit '/static_pages/about'
       expect(page).to have_content('About Us')
     end
-    it "should have the title 'About Us'" do
+    it "should have the base title" do
       visit '/static_pages/about'
-      expect(page).to have_title("#{title_prefix}About Us")
+      expect(page).to have_title("#{base_title}")
     end
   end
   describe "Contact page" do
@@ -37,9 +41,9 @@ describe "Static pages" do
       visit '/static_pages/contact'
       expect(page).to have_content('Contact')
     end
-    it "should have the title 'Contact'" do
+    it "should have the base title" do
       visit '/static_pages/contact'
-      expect(page).to have_title("#{title_prefix}Contact")
+      expect(page).to have_title("#{base_title}")
     end
   end
 end
